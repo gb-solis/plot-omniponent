c5dc66901f21f982ce6382b387d4768d336afc75
Leonardo Almeida Lessa <leonardo.a.lessa@gmail.com>
2021-09-22 01:05:30 -0300

    Added test/ to .gitignore

--- test/Spec.hs
+++ test/Spec.hs
@@ -19,11 +19,15 @@ main = do
                                     exprsUpTo = 2,
                                     allowedTerms = [Var, Const 1, Const (-1)],
                                     seed = mkStdGen 1337 }
-    let upToExprs = genUpTo [Var, Const 1, Const (-1)] 2
+    let upToExprs = genUpTo [Var] 2 :: [SymExpr]
     -- let dists = map (l2dist (-1,1) 0.01 expr) upToExprs
     -- print $ filter ((<= 1e-10) . abs . snd) $ zip upToExprs dists
     let grouped = groupBy ((<= 1e-6) ∘∘ l2dist (-1,1) 0.1) upToExprs
     let nubbed = nubBy ((<= 1e-6) ∘∘ l2dist (-1,1) 0.1) upToExprs
     let dists = map (l2dist (-1,1) 0.01 (nubbed !! 4) ) nubbed
-    print $ sortOn snd $ zip nubbed dists
+    -- Let us test if the string and expression tree representations are
+    -- (naturally) equivalent (categories)
+    let prop_equivalence expr = either (const False) (equivalentQ expr) (parseSymExpr $ show expr)
+        equivalentQ e1 e2 = l2dist (-1,1) 0.1 e1 e2 <= 1e-10
+    mapM_ print $ zip upToExprs $ map prop_equivalence upToExprs
     return ()
